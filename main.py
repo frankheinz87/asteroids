c118fe4a4010d3537415d3265ab264b37eeefda1
frankheinz87 <frankheinz1987@gmail.com>
2025-05-24 18:45:01 +0200

    implemented Player

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
-
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS, PLAYER_RADIUS
+from player import Player
 def main():
     
     print("Starting Asteroids!")
@@ -13,16 +13,15 @@ def main():
     screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock=pygame.time.Clock()
     dt=0
-    number=1
-
-    while number!=0:
+    player=Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        number-=2
         pygame.Surface.fill(screen,color=(0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt=clock.tick(60)/1000
-
+        
 if __name__=="__main__":
     main()
