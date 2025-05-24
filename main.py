d736102519955b9a0a9b9c3e48aa4faaff6dd061
frankheinz87 <frankheinz1987@gmail.com>
2025-05-24 17:56:54 +0200

    implemented 60 fps

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
 def main():
-    pygame.init()
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
     screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock=pygame.time.Clock()
+    dt=0
     number=1
+
     while number!=0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,6 +22,7 @@ def main():
         number-=2
         pygame.Surface.fill(screen,color=(0,0,0))
         pygame.display.flip()
+        dt=clock.tick(60)/1000
 
 if __name__=="__main__":
     main()
